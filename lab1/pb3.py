6bb41cb4ddba8d4bbe858758ed612adf7331cd1d
mariamaxim774 <126061959+mariamaxim774@users.noreply.github.com>
2024-10-09 12:06:08 +0300

    Delete lab1 directory

--- lab1/pb3.py
+++
@@ -1,9 +0,0 @@
-#Write a script that receives two strings and prints the number of occurrences of the first string in the second.
-
-def occurences(substring,string):
-    return string.count(substring)
-
-string=input("Input a string")
-substring=input("Input a substring")
-
-print(occurences(string,substring))
--- lab1/pb8.py
+++
@@ -1,7 +0,0 @@
-#Write a function that counts how many words exists in a text. A text is considered to be form out of words that are separated by only ONE space. For example: "I have Python exam" has 4 words.
-
-def num_of_words(string):
-    s=string.split(' ')
-    return len(s)
-
-print(num_of_words("Ana are mere"))
